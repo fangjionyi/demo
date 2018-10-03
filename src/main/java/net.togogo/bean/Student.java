039b786f4c1021f8af19ef1d902ef0cba8025160
方炯翊 <fangjiongyi@togogo.net>
2018-10-03 16:12:18 +0800

    加了一个实体类和一个控制层的类

---
+++ src/main/java/net.togogo/bean/Student.java
@@ -0,0 +1,49 @@
+package net.togogo.bean;
+
+import java.security.PrivateKey;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: 方炯翊
+ * Date: 2018-10-03
+ * Time: 16:08
+ */
+public class Student {
+    private String name;
+    private int age;
+    private String sex;
+
+    public Student() {
+    }
+
+    public Student(String name, int age, String sex) {
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+}
---
+++ src/main/java/net/togogo/controller/HomeController.java
@@ -0,0 +1,11 @@
+package net.togogo.controller;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: 方炯翊
+ * Date: 2018-10-03
+ * Time: 16:07
+ */
+public class HomeController {
+}
